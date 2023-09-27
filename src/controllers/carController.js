7375269d0b5d221dc72857d17b0dea00512beb62
Wiuver Afonso Ribeiro <wiuver.ribeiro@sicoobcentrosul.com.br>
2023-09-27 16:45:49 -0300

    Att em andamento

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -7,11 +7,11 @@ module.exports = {
   addCar: async (req, res, next) => {
     try {
       const { brand, model, description, year } = req.body;
-      const car =  new Car({
+      const car = new Car({
         brand,
         model,
         description,
-        year
+        year,
       });
       await car.save();
       return res.send({ car });
@@ -28,22 +28,51 @@ module.exports = {
       res.setHeader("Content-type", "application/json");
       res.status(200).send(cars);
     } catch (error) {
-      return res.status(400).send({ error: "Erro ao buscar  todos os carros!" });
+      return res
+        .status(400)
+        .send({ error: "Erro ao buscar  todos os carros!" });
     }
   },
 
-getCarById: async (req, res) => {
+  getCarById: async (req, res) => {
+    try {
+      const carId = req.params.carId; // Obtém o ID do parâmetro da rota
+      const car = await Car.findOne({ _id: carId }); // Busca o carro pelo ID
+
+      if (!car) {
+        return res.status(404).json({ error: "Carro não encontrado" });
+      }
+
+      return res.status(200).json({ car });
+    } catch (error) {
+      return res.status(500).json({ error: "Erro ao buscar o carro" });
+    }
+  },
+  
+editCarById: async (req, res) => {
   try {
     const carId = req.params.carId; // Obtém o ID do parâmetro da rota
-    const car = await Car.findOne({ _id: carId }); // Busca o carro pelo ID
+
+    // Verifique se o carro com o ID fornecido existe
+    const car = await Car.findById(carId);
 
     if (!car) {
       return res.status(404).json({ error: 'Carro não encontrado' });
     }
 
+    // Atualize os campos do carro com base nos dados recebidos no corpo da solicitação
+    car.brand = req.body.brand || car.brand;
+    car.model = req.body.model || car.model;
+    car.description = req.body.description || car.description;
+    car.year = req.body.year || car.year;
+
+    // Salve as alterações no banco de dados
+    await car.save();
+
     return res.status(200).json({ car });
   } catch (error) {
-    return res.status(500).json({ error: 'Erro ao buscar o carro' });
+    return res.status(500).json({ error: 'Erro ao editar o carro' });
   }
- }
 }
+
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,5 +6,6 @@ const carController = require('../controllers/carController');
 router.get('/list', carController.getAllCars);
 router.post('/register', carController.addCar);
 router.get('/:cardId', carController.getCarById);
+router.put('/:cardId', carController.editCarById);
 
 module.exports = router;
